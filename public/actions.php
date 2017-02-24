b003799e3fe27ba3b4cc76e581c9d2f44c6d8925
matt murphy <mattyhead@gmail.com>
2017-02-24 12:02:00 -0500

    'site' has become 'public'

---
+++ public/actions.php
@@ -0,0 +1,25 @@
+<?php
+if ( !class_exists('pluginabbr_public_actions') ) {
+    public class pluginabbr_public_actions {
+        public function load_styles( ) {
+            wp_enqueue_style(
+                'pvpluginabbr_public',
+                plugins_url( 'css/styles.css', __FILE__ ),
+                array(),
+                PVPLUGINABBR_VERSION,
+                'screen'
+            );
+        }
+
+        public function load_scripts( ) {
+            wp_enqueue_script(
+                'pvpluginabbr_public',
+                plugins_url( 'js/main.js', __FILE__ ),
+                array( 'query' ),
+                PVPLUGINABBR_VERSION
+            );
+        }
+    }
+}
+add_action( 'init', pluginabbr_public_actions::load_styles( ) );
+add_action( 'init', pluginabbr_public_actions::load_scripts( ) );
